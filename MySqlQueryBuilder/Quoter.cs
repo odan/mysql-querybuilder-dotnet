8cd064c10e12a29d7859ea288c49c68715610373
odan <odan@users.noreply.github.com>
2018-08-05 18:42:12 +0200

    Added Quoter

---
+++ MySqlQueryBuilder/Quoter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MySqlQueryBuilder
+{
+    public class Quoter
+    {
+
+        public string Quote(string value)
+        {
+            if (value == null)
+            {
+                return "NULL";
+            }
+
+            string result = MySql.Data.MySqlClient.MySqlHelper.EscapeString(value);
+
+            if (result == null)
+            {
+                return "NULL";
+            };
+
+            result = Regex.Replace(result, @"[\x00\b\n\r\t\cZ]", delegate (Match match)
+            {
+                string v = match.Value;
+                switch (v)
+                {
+                    case "\x00":            // ASCII NUL (0x00) character
+                        return "\\0";
+                    case "\b":              // BACKSPACE character
+                        return "\\b";
+                    case "\n":              // NEWLINE (linefeed) character
+                        return "\\n";
+                    case "\r":              // CARRIAGE RETURN character
+                        return "\\r";
+                    case "\t":              // TAB
+                        return "\\t";
+                    case "\u001A":          // Ctrl-Z
+                        return "\\Z";
+                    default:
+                        return "\\" + v;
+                }
+            });
+
+            return "'" + result + "'";
+        }
+
+        public string Quote(bool value)
+        {
+            return this.Quote(value ? "1": "0");
+        }
+
+        public string Quote(object value)
+        {
+            if (value == null)
+            {
+                return "NULL";
+            }
+
+            return this.Quote(value.ToString());
+        }
+
+        public string QuoteName(string name)
+        {
+            name = name.Trim();
+
+            string[] separators = { " AS ", " ", " as " };
+
+            foreach (string seperator in separators)
+            {
+                int position = name.IndexOf(seperator);
+
+                if (position >= 0)
+                {
+                    return this.QuoteNameWithSeparator(name, seperator, position);
+                }
+            }
+
+            return this.QuoteIdentifier(name);
+        }
+
+        protected string QuoteNameWithSeparator(string name, string seperator, int position)
+        {
+            string part1 = this.QuoteName(name.Substring(0, position));
+            string part2 = this.QuoteIdentifier(name.Substring(position + seperator.Length));
+
+            return String.Format("{0} AS {1}", part1, part2);
+        }
+
+        public string QuoteIdentifier(string name)
+        {
+            name = name.Trim();
+
+            string[] parts = name.Split(".");
+            string seperator = "";
+            string result = "";
+
+            foreach (string part in parts)
+            {
+                if (part == "*")
+                {
+                    result += seperator + part;
+                }
+                else
+                {
+                    result += seperator + "`" + part.Replace("`", "``") + "`";
+                }
+
+                seperator = ".";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MySqlQueryBuilderTest/QuoterTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using MySqlQueryBuilder;
+using Xunit;
+
+namespace MySqlQueryBuilderTest
+{
+    public class QuoterTest : BaseTest
+    {
+        [Fact]
+        public void TestQuoter()
+        {
+            Quoter quoter = new Quoter();
+
+            Assert.Equal("NULL", quoter.Quote(null));
+            Assert.Equal("'\\0'", quoter.Quote("\x00"));
+            Assert.Equal("'0'", quoter.Quote(0));
+            Assert.Equal("'0'", quoter.Quote("0"));
+            Assert.Equal("'0'", quoter.Quote(false));
+            Assert.Equal("'1'", quoter.Quote(true));
+            Assert.Equal("'-1'", quoter.Quote(-1));
+            Assert.Equal("'abc123'", quoter.Quote("abc123"));
+            Assert.Equal("'öäüÖÄÜß'", quoter.Quote("öäüÖÄÜß"));
+            Assert.Equal("'?'", quoter.Quote("?"));
+            Assert.Equal("':'", quoter.Quote(":"));
+            Assert.Equal("'\\''", quoter.Quote("'"));
+            Assert.Equal("'\\\"'", quoter.Quote("\""));
+            Assert.Equal("'\\\\'", quoter.Quote("\\"));
+            Assert.Equal("'\\0'", quoter.Quote("\x00"));
+            Assert.Equal("'\\Z'", quoter.Quote("\x1a"));
+            Assert.Equal("'\\n'", quoter.Quote("\n"));
+            Assert.Equal("'\\r'", quoter.Quote("\r"));
+            Assert.Equal("','", quoter.Quote(","));
+            Assert.Equal("'\\','", quoter.Quote("',"));
+            //Assert.Equal("'`'", quoter.Quote("`"));
+            Assert.Equal("'\\`'", quoter.Quote("`"));
+            Assert.Equal("'%s'", quoter.Quote("%s"));
+            //Assert.Equal("'\\%s'", quoter.Quote("%s"));
+            Assert.Equal("'Naughty \\' string'", quoter.Quote("Naughty ' string"));
+            Assert.Equal("'@þÿ€'", quoter.Quote("@þÿ€"));
+            // Injection patterns
+            Assert.Equal("'\\' OR \\'\\'=\\''", quoter.Quote("' OR ''='"));
+            Assert.Equal("'1\\' or \\'1\\' = \\'1'", quoter.Quote("1' or '1' = '1"));
+            Assert.Equal("'1\\' or \\'1\\' = \\'1\\'))/*'", quoter.Quote("1' or '1' = '1'))/*"));
+        }
+
+        [Fact]
+        public void TestQuoteName()
+        {
+            Quoter quoter = new Quoter();
+
+            Assert.Equal("`field`", quoter.QuoteName("field"));
+            Assert.Equal("`table`.`field`", quoter.QuoteName("table.field"));
+            Assert.Equal("`db`.`table`.`field`", quoter.QuoteName("db.table.field"));
+
+            Assert.Equal("`field` AS `f1`", quoter.QuoteName("field AS f1"));
+            Assert.Equal("`table`.`field` AS `f1`", quoter.QuoteName("table.field AS f1"));
+            Assert.Equal("`db`.`table`.`field` AS `f1`", quoter.QuoteName("db.table.field AS f1"));
+
+            Assert.Equal("`field` AS `f1`", quoter.QuoteName("field f1"));
+            Assert.Equal("`table`.`field` AS `f1`", quoter.QuoteName("table.field f1"));
+            Assert.Equal("`db`.`table`.`field` AS `f1`", quoter.QuoteName("db.table.field f1"));
+
+            Assert.Equal("*", quoter.QuoteName("*"));
+            Assert.Equal("`table`.*", quoter.QuoteName("table.*"));
+            Assert.Equal("`db`.`table`.*", quoter.QuoteName("db.table.*"));
+        }
+    }
+}
